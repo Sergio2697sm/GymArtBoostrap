6094387d44a054689dceb096b6f57c641eba3c39
sergio2697 <sergiomartinez2m@gmail.com>
2020-05-27 18:09:14 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer class="d-flex align-items-end">
+    © 2020 Sergio Martínez Martínez. All rights reserved.
+</footer>
---
+++ header.php
@@ -0,0 +1,29 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><img src="./imagenes/logo1.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="float-sm-right collapse navbar-collapse navbar-center" id="navbarSupportedContent">
+            <ul class="nav navbar-nav navbar-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">SERVICIO</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">QUIENES SOMOS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">CONTACTO</a>
+                </li>
+            </ul>
+        </div>
+        <div class="float-sm-center d-flex flex-column">
+            <p>Bienvenido,usuario</p>
+            <button type="submit" class="btn btn-danger">
+                <span class="glyphicon glyphicon-log-out"></span> Cerrar Sesión
+            </button>
+        </div>
+
+    </nav>
+</header>
